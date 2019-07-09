1e4146d875462654e3b7e6998f9470b94912f56e
Vincent Nguyen <vpvnguyen@gmail.com>
2019-07-08 23:09:17 -0700

    adding the rest of components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,8 +5,26 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 // adding bootstrap
 import "bootstrap/dist/css/bootstrap.css";
+// adding counter class
+import Counter from "./components/counter";
+// adding myName
+import MyName from "./components/MyName";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+ReactDOM.render(<Counter />, document.getElementById("hello"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+ReactDOM.render(<MyName />, document.getElementById("name"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
