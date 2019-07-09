368f214cd0d3fad4a4634a8250e626c77950d507
Vincent Nguyen <vpvnguyen@gmail.com>
2019-07-08 21:12:43 -0700

    added dive text as a component

---
+++ my-app/src/components/MyName.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+// creating a component, when rendered will return hello world
+class MyName extends Component {
+  render() {
+    return <div>Yo yo</div>;
+  }
+}
+
+export default MyName;
